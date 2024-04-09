2f05b13daf1b212d111d97f34815c7841c4a3088
84347 <84347@LAPTOP-C95ULGJ0>
2024-04-09 14:16:16 +0700

    [add] lab-02

--- lab-02/cipher/railfence/railfence_cipher.py
+++ lab-02/cipher/railfence/railfence_cipher.py
@@ -19,34 +19,34 @@ class RailFenceCiher:
         return cipher_text
     
     
-    def rail_fence_decrypt (self, cipher_text, num_rails):
+    def rail_fence_decrypt(self, cipher_text : str, num_rails: int):
         rail_lengths = [0] * num_rails
         rail_index = 0
         direction = 1
-        for _ in range (len (cipher_text)):
-            rail_lengths[rail_index] += 1
+            
+        for _ in range (len(cipher_text)):
+            rail_lengths [rail_index] += 1
             if rail_index == 0:
                 direction = 1
             elif rail_index == num_rails - 1:
                 direction = -1
             rail_index += direction
-            
-        rails = []
+        
+        rails = []       
         start = 0
-        for length in rail_lengths:
+        for length in rail_lengths: 
             rails.append(cipher_text[start:start + length])
             start += length
-            plain_text = ""
-            rail_index=0
-            direction = 1
-                        
-            for _ in range (len(cipher_text)):
-                plain_text += rails[rail_index] [0]
-                rails[rail_index] = rails [rail_index] [1:]
-                if rail_index == 0:
-                    direction = 1
-                elif rail_index == num_rails - 1:
-                    direction = -1
-                rail_index += direction
+        plain_text = ""
+        rail_index = 0
+        direction = 1
+        for _ in range (len (cipher_text)):
+            plain_text += rails [rail_index] [0]
+            rails [rail_index] = rails [rail_index] [1:]
+            if rail_index == 0:
+                direction = 1
+            elif rail_index == num_rails -1:
+                direction = -1
+            rail_index += direction
                 
-            return plain_text
+        return plain_text
