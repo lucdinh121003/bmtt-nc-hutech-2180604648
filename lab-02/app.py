600bb1774fea470294a65afa0930887b8775ba9d
84347 <84347@LAPTOP-C95ULGJ0>
2024-04-09 14:54:10 +0700

    [add] lab-02

--- lab-02/app.py
+++ lab-02/app.py
@@ -14,7 +14,7 @@ def caesar():
 
 @app.route("/encrypt", methods=['POST'])
 def caesar_encrypt():
-    text = request.form('inputPlainText')
+    text = request.form['inputPlainText']
     key = int(request.form['inputKeyPlain'])
     Caesar = CaesarCipher()
     encrypted_text = Caesar.encrypt_text(text, key)
@@ -22,10 +22,10 @@ def caesar_encrypt():
 
 @app.route("/decrypt", methods=['POST'])
 def caesar_decrypt():
-    text = request.form('inputCipherText')
+    text = request.form['inputCipherText']
     key = int(request.form['inputKeyCipher'])
     Caesar = CaesarCipher()
-    decrypted_text = Caesar.encrypt_text(text, key)
+    decrypted_text = Caesar.decrypt_text(text, key)
     return f"text: {text}<br/>key: {key}<br/>decrypted text:{decrypted_text}"
 
 
